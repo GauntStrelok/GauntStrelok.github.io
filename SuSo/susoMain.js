72b0544908ddfe1d92578a50668eb0cbf3582f4e
Pablo <pablo.gotelli@shipeala.com>
2016-10-23 19:02:06 -0300

    Signed-off-by: Pablo <pablo.gotelli@shipeala.com>

--- SuSo/susoMain.js
+++ SuSo/susoMain.js
@@ -14,6 +14,16 @@ function initSuSo() {
 				if(k != currentCell.column) currentCell.rowCells.push(suSoCells[i][k])
 				if(k != currentCell.row)currentCell.columnCells.push(suSoCells[k][j])
 			}
+			var firstPosX = Math.floor(i/3) * 3;
+			var firstPosY = Math.floor(j/3) * 3;
+			for (var x = 3; x--;) {
+				for (var y = 3; y--;) {
+					//firstPosX + x es igual a la posicion de la celda del bloque, al igual que las posiciones de las celdas y.
+					if (firstPosX + x != i || firstPosY + y != j) {
+						currentCell.blockCells.push(suSoCells[firstPosX + x][firstPosY + y]);
+					}
+				}
+			}
 		}
 	}
 }
@@ -31,13 +41,12 @@ function solve() {
 		}
 	}
 	return suSoCells;
-	
 }
 
 function filterElements(cell, index, array, currentCell) {
 	if(cell.value) {
 		removePossible(currentCell, value);
-		return false; //tengo que remover el elemento de mis columnas o filas y, eliminarlo de los valores posibles, esto lo hago para una version futura 
+		return false; //tengo que remover el elemento de mis columnas o filas y, eliminarlo de los valores posibles, esto lo hago para una version futura
 	}
 	return true;
 }
@@ -57,6 +66,7 @@ function SuSoCell(row, column) {
     this.coords = [row,column];
     this.rowCells = [];
     this.columnCells = [];
+	 this.blockCells = [];
     //update row, but the only thing it does, is chekcking the other cells to remove those with a value, and remove that values from possible values
     //so it can minimize the effort the next time you want to update this row and column
     this.updateRow = function() {
