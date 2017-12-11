4e000446bf1680e5fb720b85e3834e5bed5dc9cb
pgotelli <pgotelli@voiceovernetinc.com>
2017-12-11 17:13:26 -0300

    Signed-off-by: pgotelli <pgotelli@voiceovernetinc.com>

--- pegoTransformer/transformer.js
+++ pegoTransformer/transformer.js
@@ -1,20 +1,4 @@
 function addPrependToWords(prependText, text) {
-  var caos = text.replace("[a-zA-Z]+", prependText + "$&")
-  console.log(caos);
-  console.log(text);
-  return caos;
-  var words = text.split(" ");
-  words.map(word => {
-    let firstCharcode = word.charCodeAt(0);
-    let secondCharcode = word.charCodeAt(1);
-    if((firstCharcode >= 65 && firstCharcode <= 90) || (firstCharcode >= 97 && firstCharcode <= 122)){
-      return prependText + word;
-    } else if((secondCharcode >= 65 && secondCharcode <= 90) || (secondCharcode >= 97 && secondCharcode <= 122)) {
-      return word[0] + prependText + word.substring(1);
-    } else {
-      console.log("error with word, " + word);
-      return word;
-    }
-  });
-  return words.join(" ");
+  var r = text.replace(/(pego)?[a-zA-Z]+/g, function($0,$1){ return $1?$0:prependText+$0});
+  return r;
 }
