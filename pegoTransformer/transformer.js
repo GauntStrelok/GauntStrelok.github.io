7d36ee6c7dea0747752192ac9c381f86045ef149
pgotelli <pgotelli@voiceovernetinc.com>
2017-12-11 17:27:13 -0300

    Signed-off-by: pgotelli <pgotelli@voiceovernetinc.com>

--- pegoTransformer/transformer.js
+++ pegoTransformer/transformer.js
@@ -2,6 +2,3 @@ function addPrependToWords(prependText, text) {
   var r = text.replace(/(pego)?[a-zA-Z]+/g, function($0,$1){ return $1?$0:prependText+$0});
   return r;
 }
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
