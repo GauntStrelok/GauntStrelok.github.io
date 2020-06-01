9cdb9aba95a9e14c02d1da76e823859bacdbfe83
Pablo Gotelli <pablonahuelgotelli@gmail.com>
2020-05-31 22:12:04 -0300

    Changed homepage to reflect real url to search files

---
+++ bookCatalogue/static/precache-manifest.753d6e307331d4af1c56dad3b98a9912.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5a2c7b9ebd8e8585b02045b69797fd",
+    "url": "/bookCatalogue/index.html"
+  },
+  {
+    "revision": "36b1903aa31777790f18",
+    "url": "/bookCatalogue/static/css/main.528989f9.chunk.css"
+  },
+  {
+    "revision": "1621582fca0794acdb9a",
+    "url": "/bookCatalogue/static/js/2.b57da043.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/bookCatalogue/static/js/2.b57da043.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36b1903aa31777790f18",
+    "url": "/bookCatalogue/static/js/main.9dbae41b.chunk.js"
+  },
+  {
+    "revision": "6a1cfb20e47667f1a7cc",
+    "url": "/bookCatalogue/static/js/runtime-main.a935312d.js"
+  }
+]);
